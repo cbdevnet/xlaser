45ca92ff303cee8d0d03bd9a52a3855745d7d124
cbdev <cb@cbcdn.com>
2016-06-14 17:49:09 +0200

    Add network header and config

--- xlaser.c
+++ xlaser.c
@@ -1,5 +1,18 @@
 #include "xlaser.h"
 
+int usage(char* fn){
+	printf("xlaser - Whatever\n");
+	printf("Usage:\n");
+	printf("\t%s <dmx address> <bindhost>", fn);
+	return EXIT_FAILURE;
+}
+
 int main(int argc, char** argv){
+	if(argc < 3){
+		exit(usage(argv[0]));
+	}
+
+
+
 	return 0;
 }
--- xlaser.h
+++ xlaser.h
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <stdlib.h>
+#include <stdint.h>
 //#include <unistd.h>
 //#include <fcntl.h>
 //#include <ctype.h>
@@ -21,3 +23,31 @@ typedef struct /*_XDATA*/ {
 	Atom wm_delete;
 	X_FDS xfds;
 } XRESOURCES;
+
+typedef struct /*XLASER_CFG*/ {
+	uint16_t dmx_address;
+	bool windowed;
+	unsigned window_width;
+	unsigned window_height;
+	unsigned x_offset;
+	unsigned y_offset;
+	unsigned xmax;
+	unsigned ymax;
+	char* bindhost;
+	//char* backgnd_image;
+	//char** gobos;
+	//chanmap?
+} CONFIG;
+
+// CHANNELS
+// X 16bit
+// Y 16bit
+// R G B 24bit
+// Dimmer
+// Shutter
+// Gobo
+// Focus
+// Rotation Abs
+// Rotation Speed
+
+#include "network.h"
